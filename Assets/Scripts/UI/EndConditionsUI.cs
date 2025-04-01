39e9eec8ab340a5a2f9665eab28321f8d953d111
RussellYes <russellnadin@gmail.com>
2025-04-01 15:24:24 -0600

    End text has visual and SFX cues.

--- Assets/Scripts/UI/EndConditionsUI.cs
+++ Assets/Scripts/UI/EndConditionsUI.cs
@@ -8,34 +8,45 @@ public class EndConditionsUI : MonoBehaviour
 {
     private ScoreManager scoreManager;
     private GameManager gameManager;
+    private SFXManager sFXManager;
 
     [SerializeField] private bool isWin;
     [SerializeField] private Image winBackground;
     [SerializeField] private Button homeButton;
     [SerializeField] private TextMeshProUGUI winText;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private GameObject scoreTextShineBar;
     [SerializeField] private TextMeshProUGUI obstaclesDestroyedText;
+    [SerializeField] private GameObject obstaclesDestroyedShineBar;
     [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private GameObject timeShineBar;
     [SerializeField] private Sprite winSprite;
     [SerializeField] private Sprite loseSprite;
     [SerializeField] private Color loseColor;
-
-
+    [SerializeField] private AudioClip[] textMovementSfx;
+    [SerializeField] private float textAppearDelay;
+    [SerializeField] private float sFXDelay;
 
     private void Awake()
     {
         scoreManager = FindAnyObjectByType<ScoreManager>();
         gameManager = FindAnyObjectByType<GameManager>();
+        sFXManager = FindAnyObjectByType<SFXManager>();
 
-        homeButton.onClick.AddListener(() => {
+        // Initially hide all text elements and their shine bars
+        winText.gameObject.SetActive(false);
+        scoreText.gameObject.SetActive(false);
+        if (scoreTextShineBar != null) scoreTextShineBar.SetActive(false);
+        obstaclesDestroyedText.gameObject.SetActive(false);
+        if (obstaclesDestroyedShineBar != null) obstaclesDestroyedShineBar.SetActive(false);
+        timeText.gameObject.SetActive(false);
+        if (timeShineBar != null) timeShineBar.SetActive(false);
+
+        homeButton.onClick.AddListener(() =>
+        {
             Loader.Load(Loader.Scene.MainMenuScene);
             Debug.Log("Loading Scene");
         });
-    }
-
-    private void Start()
-    {
-
     }
 
     private void OnEnable()
@@ -64,7 +75,6 @@ public class EndConditionsUI : MonoBehaviour
             }
             else
             {
-                //winBackground.sprite = loseSprite; //Activate this code and remove the next line when endLose art is complete
                 winBackground.color = loseColor;
                 winText.text = "You lose";
             }
@@ -79,10 +89,45 @@ public class EndConditionsUI : MonoBehaviour
             int minutes = Mathf.FloorToInt(gameManager.GameTime / 60);
             int seconds = Mathf.FloorToInt(gameManager.GameTime % 60);
             timeText.text = $"Time: {minutes:00}:{seconds:00}";
+
+            StartCoroutine(ShowTextsWithDelay());
         }
         else
         {
             Debug.Log("EndConditionsUI can't find ScoreManager or GameManager");
+        }
+    }
+
+    private IEnumerator ShowTextsWithDelay()
+    {
+        // Show win/lose text and shine bar
+        winText.gameObject.SetActive(true);
+        yield return new WaitForSecondsRealtime(textAppearDelay);
+
+        // Show score text and shine bar
+        scoreText.gameObject.SetActive(true);
+        if (scoreTextShineBar != null) scoreTextShineBar.SetActive(true);
+        PlayRandomTextSfx();
+        yield return new WaitForSecondsRealtime(textAppearDelay);
+
+        // Show obstacles destroyed text and shine bar
+        obstaclesDestroyedText.gameObject.SetActive(true);
+        if (obstaclesDestroyedShineBar != null) obstaclesDestroyedShineBar.SetActive(true);
+        PlayRandomTextSfx();
+        yield return new WaitForSecondsRealtime(textAppearDelay);
+
+        // Show time text and shine bar
+        timeText.gameObject.SetActive(true);
+        if (timeShineBar != null) timeShineBar.SetActive(true);
+        PlayRandomTextSfx();
+    }
+
+    private void PlayRandomTextSfx()
+    {
+        if (textMovementSfx != null && textMovementSfx.Length > 0 && sFXManager != null)
+        {
+            int randomIndex = Random.Range(0, textMovementSfx.Length);
+            sFXManager.PlaySFX(textMovementSfx[randomIndex]);
         }
     }
 }
