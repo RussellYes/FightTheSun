02e8e440680d7c0e7e92895a361a04bbfe335853
RussellYes <russellnadin@gmail.com>
2025-06-16 15:39:54 -0600

    Added metal, rareMetal variables, costs, and icons to ShipUpgradesUI.

--- Assets/Scripts/UI/ShipUpgradesUI.cs
+++ Assets/Scripts/UI/ShipUpgradesUI.cs
@@ -17,8 +17,9 @@ public class ShipUpgradesUI : MonoBehaviour
     [SerializeField] private AudioClip[] shipUpgradeMenuOpenCloseSFX;
 
     [Header("Memory Score")]
-    private float memoryScore;
     [SerializeField] private TextMeshProUGUI memoryScoreText;
+    [SerializeField] private TextMeshProUGUI metalScoreText;
+    [SerializeField] private TextMeshProUGUI rareMetalScoreText;
 
     [Header("Upgrade Buttons")]
     public Button engineeringButton;
@@ -27,24 +28,60 @@ public class ShipUpgradesUI : MonoBehaviour
     public Button miningButton;
     public Button roboticsButton;
     public Button combatButton;
+
+    [Header("Engineering Upgrades")]
     [SerializeField] private TextMeshProUGUI engineeringText;
+    [SerializeField] private TextMeshProUGUI engineeringMemoryCostText;
+    private float engineeringMemoryCost;
+    [SerializeField] private TextMeshProUGUI engineeringMetalCostText;
+    private float engineeringMetalCost;
+    [SerializeField] private TextMeshProUGUI engineeringRareMetalCostText;
+    private float engineeringRareMetalCost;
+
+    [Header("Piloting Upgrades")]
     [SerializeField] private TextMeshProUGUI pilotingText;
+    [SerializeField] private TextMeshProUGUI pilotingMemoryCostText;
+    private float pilotingMemoryCost;
+    [SerializeField] private TextMeshProUGUI pilotingMetalCostText;
+    private float pilotingMetalCost;
+    [SerializeField] private TextMeshProUGUI pilotingRareMetalCostText;
+    private float pilotingRareMetalCost;
+
+    [Header("Mechanics Upgrades")]
     [SerializeField] private TextMeshProUGUI mechanicsText;
+    [SerializeField] private TextMeshProUGUI mechanicsMemoryCostText;
+    private float mechanicsMemoryCost;
+    [SerializeField] private TextMeshProUGUI mechanicsMetalCostText;
+    private float mechanicsMetalCost;
+    [SerializeField] private TextMeshProUGUI mechanicsRareMetalCostText;
+    private float mechanicsRareMetalCost;
+
+    [Header("Mining Upgrades")]
     [SerializeField] private TextMeshProUGUI miningText;
+    [SerializeField] private TextMeshProUGUI miningMemoryCostText;
+    private float miningMemoryCost;
+    [SerializeField] private TextMeshProUGUI miningMetalCostText;
+    private float miningMetalCost;
+    [SerializeField] private TextMeshProUGUI miningRareMetalCostText;
+    private float miningRareMetalCost;
+
+    [Header("Robotics Upgrades")]
     [SerializeField] private TextMeshProUGUI roboticsText;
+    [SerializeField] private TextMeshProUGUI roboticsMemoryCostText;
+    private float roboticsMemoryCost;
+    [SerializeField] private TextMeshProUGUI roboticsMetalCostText;
+    private float roboticsMetalCost;
+    [SerializeField] private TextMeshProUGUI roboticsRareMetalCostText;
+    private float roboticsRareMetalCost;
+
+    [Header("Combat Upgrades")]
     [SerializeField] private TextMeshProUGUI combatText;
-    [SerializeField] private TextMeshProUGUI engineeringCostText;
-    [SerializeField] private TextMeshProUGUI pilotingCostText;
-    [SerializeField] private TextMeshProUGUI mechanicsCostText;
-    [SerializeField] private TextMeshProUGUI miningCostText;
-    [SerializeField] private TextMeshProUGUI roboticsCostText;
-    [SerializeField] private TextMeshProUGUI combatCostText;
-    private float engineeringCost;
-    private float pilotingCost;
-    private float mechanicsCost;
-    private float miningCost;
-    private float roboticsCost;
-    private float combatCost;
+    [SerializeField] private TextMeshProUGUI combatMemoryCostText;
+    private float combatMemoryCost;
+    [SerializeField] private TextMeshProUGUI combatMetalCostText;
+    private float combatMetalCost;
+    [SerializeField] private TextMeshProUGUI combatRareMetalCostText;
+    private float combatRareMetalCost;
 
     private void Start()
     {
@@ -61,16 +98,10 @@ public class ShipUpgradesUI : MonoBehaviour
     {
         if (DataPersister.Instance != null && DataPersister.Instance.CurrentGameData != null)
         {
-            if (DataPersister.Instance.CurrentGameData.playerData.Count > 0)
-                memoryScore = DataPersister.Instance.CurrentGameData.playerData[0].playerMemoryScore;
-            else
+            if (DataPersister.Instance.CurrentGameData.playerData.Count == 0)
             {
-                // Initialize if empty
                 DataPersister.Instance.CurrentGameData.playerData.Add(new PlayerSaveData());
-                memoryScore = 0;
             }
-
-            Debug.Log($"Loaded memoryScore: {memoryScore}");
         }
         UpdateMemoryAndSkillsText();
     }
@@ -162,17 +193,29 @@ public class ShipUpgradesUI : MonoBehaviour
 
     public float GetMemoryScore()
     {
-        return memoryScore;
+        return DataPersister.Instance.CurrentGameData.playerData[0].playerMemoryScore;
     }
-    public void UpdateMemoryText()
+
+    public float GetMetalScore()
     {
-        memoryScoreText.text = memoryScore.ToString("0") + " memories";
+        return DataPersister.Instance.CurrentGameData.totalMetal;
+    }
+
+    public float GetRareMetalScore()
+    {
+        return DataPersister.Instance.CurrentGameData.totalRareMetal;
+    }
+    public void UpdateResourcesText()
+    {
+        memoryScoreText.text = GetMemoryScore().ToString("0") + "";
+        metalScoreText.text = GetMetalScore().ToString("0") + "";
+        rareMetalScoreText.text = GetRareMetalScore().ToString("0") + "";
     }
 
 
     public void UpdateMemoryAndSkillsText()
     {
-        UpdateMemoryText();
+        UpdateResourcesText();
 
         if (playerStatsManager == null)
         {
@@ -186,105 +229,203 @@ public class ShipUpgradesUI : MonoBehaviour
         roboticsText.text = "Robotics " + playerStatsManager.RoboticsSkill.ToString("0.00");
         combatText.text = "Combat " + playerStatsManager.CombatSkill.ToString("0.00");
 
-        engineeringCost = playerStatsManager.EngineeringSkill * playerStatsManager.EngineeringSkill;
-        engineeringCostText.text = "Cost " + engineeringCost.ToString("0.00");
+        float Squared(float value) => value * value;
+        float Cube(float value) => value * value * value;
 
-        pilotingCost = playerStatsManager.PilotingSkill * playerStatsManager.PilotingSkill;
-        pilotingCostText.text = "Cost " + pilotingCost.ToString("0.00");
+        // Memory costs for upgrades
+        engineeringMemoryCost = Cube(playerStatsManager.EngineeringSkill);
+        engineeringMemoryCostText.text = engineeringMemoryCost.ToString("0.00");
 
-        mechanicsCost = playerStatsManager.MechanicsSkill * playerStatsManager.MechanicsSkill;
-        mechanicsCostText.text = "Cost " + mechanicsCost.ToString("0.00");
+        pilotingMemoryCost = Cube(playerStatsManager.PilotingSkill);
+        pilotingMemoryCostText.text = pilotingMemoryCost.ToString("0.00");
 
-        miningCost = playerStatsManager.MiningSkill * playerStatsManager.MiningSkill;
-        miningCostText.text = "Cost " + miningCost.ToString("0.00");
+        mechanicsMemoryCost = Cube(playerStatsManager.MechanicsSkill);
+        mechanicsMemoryCostText.text = mechanicsMemoryCost.ToString("0.00");
 
-        roboticsCost = playerStatsManager.RoboticsSkill * playerStatsManager.RoboticsSkill;
-        roboticsCostText.text = "Cost " + roboticsCost.ToString("0.00");
+        miningMemoryCost = Cube(playerStatsManager.MiningSkill);
+        miningMemoryCostText.text = miningMemoryCost.ToString("0.00");
 
-        combatCost = playerStatsManager.CombatSkill * playerStatsManager.CombatSkill;
-        combatCostText.text = "Cost " + combatCost.ToString("0.00");
+        roboticsMemoryCost = Cube(playerStatsManager.RoboticsSkill);
+        roboticsMemoryCostText.text = roboticsMemoryCost.ToString("0.00");
+
+        combatMemoryCost = Cube(playerStatsManager.CombatSkill);
+        combatMemoryCostText.text = combatMemoryCost.ToString("0.00");
+
+        // Metal costs f0r upgrades
+        engineeringMetalCost = Squared(playerStatsManager.EngineeringSkill);
+        if (engineeringMetalCost < 1.5)
+        {
+            engineeringMetalCost = 0;
+        }
+        engineeringMetalCostText.text = engineeringMetalCost.ToString("0.00");
+
+        pilotingMetalCost = Squared(playerStatsManager.PilotingSkill);
+        if (pilotingMetalCost < 1.5)
+        {
+            pilotingMetalCost = 0;
+        }
+        pilotingMetalCostText.text = pilotingMetalCost.ToString("0.00");
+
+        mechanicsMetalCost = Squared(playerStatsManager.MechanicsSkill);
+        if (mechanicsMetalCost < 1.5)
+        {
+            mechanicsMetalCost = 0;
+        }
+        mechanicsMetalCostText.text = mechanicsMetalCost.ToString("0.00");
+
+        miningMetalCost = Squared(playerStatsManager.MiningSkill);
+        if (miningMetalCost < 1.5)
+        {
+            miningMetalCost = 0;
+        }
+        miningMetalCostText.text = miningMetalCost.ToString("0.00");
+
+        roboticsMetalCost = Squared(playerStatsManager.RoboticsSkill);
+        if (roboticsMetalCost < 1.5)
+        {
+            roboticsMetalCost = 0;
+        }
+        roboticsMetalCostText.text = roboticsMetalCost.ToString("0.00");
+
+        combatMetalCost = Squared(playerStatsManager.CombatSkill);
+        if (combatMetalCost < 1.5)
+        {
+            combatMetalCost = 0;
+        }
+        combatMetalCostText.text = combatMetalCost.ToString("0.00");
+
+        // Rare Metal costs for upgrades
+        engineeringRareMetalCost = Squared(playerStatsManager.EngineeringSkill);
+        if (engineeringRareMetalCost < 2)
+        {
+            engineeringRareMetalCost = 0;
+        }
+        engineeringRareMetalCost = engineeringRareMetalCost / 2;
+        engineeringRareMetalCostText.text = engineeringRareMetalCost.ToString("0.00");
+
+        pilotingRareMetalCost = Squared(playerStatsManager.PilotingSkill);
+        if (pilotingRareMetalCost < 2)
+        {
+            pilotingRareMetalCost = 0;
+        }
+        pilotingRareMetalCost = pilotingRareMetalCost / 2;
+        pilotingRareMetalCostText.text = pilotingRareMetalCost.ToString("0.00");
+
+        mechanicsRareMetalCost = Squared(playerStatsManager.MechanicsSkill);
+        if (mechanicsRareMetalCost < 2)
+        {
+            mechanicsRareMetalCost = 0;
+        }
+        mechanicsRareMetalCost = mechanicsRareMetalCost / 2;
+        mechanicsRareMetalCostText.text = mechanicsRareMetalCost.ToString("0.00");
+
+        miningRareMetalCost = Squared(playerStatsManager.MiningSkill);
+        if (miningRareMetalCost < 2)
+        {
+            miningRareMetalCost = 0;
+        }
+        miningRareMetalCost = miningRareMetalCost / 2;
+        miningRareMetalCostText.text = miningRareMetalCost.ToString("0.00");
+
+        roboticsRareMetalCost = Squared(playerStatsManager.RoboticsSkill);
+        if (roboticsRareMetalCost < 2)
+        {
+            roboticsRareMetalCost = 0;
+        }
+        roboticsRareMetalCost = roboticsRareMetalCost / 2;
+        roboticsRareMetalCostText.text = roboticsRareMetalCost.ToString("0.00");
+
+        combatRareMetalCost = Squared(playerStatsManager.CombatSkill);
+        if (combatRareMetalCost < 2)
+        {
+            combatRareMetalCost = 0;
+        }
+        combatRareMetalCost = combatRareMetalCost / 2;
+        combatRareMetalCostText.text = combatRareMetalCost.ToString("0.00");
+
     }
 
     private void BuyEngineering()
     {
-        if (memoryScore >= engineeringCost)
+        if (GetMemoryScore() >= engineeringMemoryCost && GetMetalScore() >= engineeringMetalCost && GetRareMetalScore() >= engineeringRareMetalCost)
         {
-            memoryScore -= engineeringCost;
+            AddMemory(engineeringMemoryCost);
+            AddMetal(engineeringMetalCost);
+            AddRareMetal(engineeringRareMetalCost);
             playerStatsManager.MultiplyEngineeringSkill();
-            SaveMemoryAndSkills();
             UpdateMemoryAndSkillsText();
         }
     }
     private void BuyPiloting()
     {
-        if (memoryScore >= pilotingCost)
+        if (GetMemoryScore() >= pilotingMemoryCost && GetMetalScore() >= pilotingMetalCost && GetRareMetalScore() >= pilotingRareMetalCost)
         {
-            memoryScore -= pilotingCost;
+            AddMemory(pilotingMemoryCost);
+            AddMetal(pilotingMetalCost);
+            AddRareMetal(pilotingRareMetalCost);
             playerStatsManager.MultiplyPilotingSkill();
-            SaveMemoryAndSkills();
             UpdateMemoryAndSkillsText();
         }
     }
     private void BuyMechanics()
     {
-        if (memoryScore >= mechanicsCost)
+        if (GetMemoryScore() >= mechanicsMemoryCost && GetMetalScore() >= mechanicsMetalCost && GetRareMetalScore() >= mechanicsRareMetalCost)
         {
-            memoryScore -= mechanicsCost;
+            AddMemory(mechanicsMemoryCost);
+            AddMetal(mechanicsMetalCost);
+            AddRareMetal(mechanicsRareMetalCost);
             playerStatsManager.MultiplyMechanicsSkill();
-            SaveMemoryAndSkills();
             UpdateMemoryAndSkillsText();
         }
     }
     private void BuyMining()
     {
-        if (memoryScore >= miningCost)
+        if (GetMemoryScore() >= miningMemoryCost && GetMetalScore() >= miningMetalCost && GetRareMetalScore() >= miningRareMetalCost)
         {
-            memoryScore -= miningCost;
+            AddMemory(miningMemoryCost);
+            AddMetal(miningMetalCost);
+            AddRareMetal(miningRareMetalCost);
             playerStatsManager.MultiplyMiningSkill();
-            SaveMemoryAndSkills();
             UpdateMemoryAndSkillsText();
         }
     }
     private void BuyRobotics()
     {
-        if (memoryScore >= roboticsCost)
+        if (GetMemoryScore() >= roboticsMemoryCost && GetMetalScore() >= roboticsMetalCost && GetRareMetalScore() >= roboticsRareMetalCost)
         {
-            memoryScore -= roboticsCost;
+            AddMemory(roboticsMemoryCost);
+            AddMetal(roboticsMetalCost);
+            AddRareMetal(roboticsRareMetalCost);
             playerStatsManager.MultiplyRoboticsSkill();
-            SaveMemoryAndSkills();
             UpdateMemoryAndSkillsText();
         }
     }
     private void BuyCombat()
     {
-        if (memoryScore >= combatCost)
+        if (GetMemoryScore() >= combatMemoryCost && GetMetalScore() >= combatMetalCost && GetRareMetalScore() >= combatRareMetalCost)
         {
-            memoryScore -= combatCost;
+            AddMemory(combatMemoryCost);
+            AddMetal(combatMetalCost);
+            AddRareMetal(combatRareMetalCost);
             playerStatsManager.MultiplyCombatSkill();
-            SaveMemoryAndSkills();
             UpdateMemoryAndSkillsText();
         }
     }
-
-    private void SaveMemoryAndSkills()
+    public void AddMemory(float amount)
     {
-        if (DataPersister.Instance != null && DataPersister.Instance.CurrentGameData != null)
-        {
-            // Ensure player data exists
-            if (DataPersister.Instance.CurrentGameData.playerData.Count == 0)
-            {
-                DataPersister.Instance.CurrentGameData.playerData.Add(new PlayerSaveData());
-            }
+        DataPersister.Instance.CurrentGameData.playerData[0].playerMemoryScore += amount;
+        DataPersister.Instance.SaveCurrentGame();
+    }
+    public void AddMetal(float amount)
+    {
+        DataPersister.Instance.CurrentGameData.totalMetal += amount;
+        DataPersister.Instance.SaveCurrentGame();
+    }
 
-            // Update memory score
-            DataPersister.Instance.CurrentGameData.playerData[0].playerMemoryScore = memoryScore;
-
-            // Skills are saved by PlayerStatsManager when changed
-
-            // Save the game
-            DataPersister.Instance.SaveCurrentGame();
-            Debug.Log("Saved memory score and skills");
-        }
+    public void AddRareMetal(float amount)
+    {
+        DataPersister.Instance.CurrentGameData.totalRareMetal += amount;
+        DataPersister.Instance.SaveCurrentGame();
     }
 
 }
