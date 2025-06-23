71557326069461857a7a914cd30aaf3daa6d66bb
RussellYes <russellnadin@gmail.com>
2025-06-23 09:53:41 -0600

    MiningSkill now affects MiningClaw's mining time.

--- Assets/Scripts/Projectiles/CreateLoot.cs
+++ Assets/Scripts/Projectiles/CreateLoot.cs
@@ -6,11 +6,32 @@ using UnityEngine;
 
 public class CreateLoot : MonoBehaviour
 {
+    PlayerStatsManager PlayerStatsManager => PlayerStatsManager.Instance;
+
     [SerializeField] private GameObject[] lootPrefab;
 
     [SerializeField] private float miningTime;
 
     public float MiningTime { get { return miningTime; }}
+
+
+    private void OnEnable()
+    {
+        DataPersister.InitializationComplete += Initialize;
+    }
+    private void OnDisable()
+    {
+        DataPersister.InitializationComplete -= Initialize;
+    }
+    private void Initialize()
+    {
+        miningTime -= PlayerStatsManager.MiningSkill;
+        if (miningTime <= 0.5f)
+        {
+            miningTime = 0.5f;
+        }
+    }
+
     public void SpawnLoot()
     {
         if (lootPrefab == null || lootPrefab.Length == 0) return;
